007aa58c0ca76aae65ae5164c99448f441e81730
overfrenzy <54790344+overfrenzy@users.noreply.github.com>
2024-10-26 19:58:10 +0900

    featured products array of products fix
    fix makes it so frontend receives an array of Product objects directly

--- app/Http/Controllers/Api/FeaturedProductsController.php
+++ app/Http/Controllers/Api/FeaturedProductsController.php
@@ -9,7 +9,10 @@ class FeaturedProductsController extends Controller
 {
     public function index()
     {
-        $featuredProducts = FeaturedProduct::with('product.category', 'product.quantityType')->get();
+        $featuredProducts = FeaturedProduct::with('product.category', 'product.quantityType')
+            ->get()
+            ->pluck('product');
+
         return response()->json($featuredProducts);
     }
 }
